b2dd7dd7f32e714e07a41d2c9c1960b319dee316
Jack625a <77822933+Jack625a@users.noreply.github.com>
2024-06-20 12:16:38 -0400

    Simplex Tkinter Completo

--- simplexInterfaz.py
+++ simplexInterfaz.py
@@ -1,6 +1,8 @@
 #Importacion de Liberias
 import tkinter as tk
 import numpy as np
+from tkinter import messagebox
+from scipy.optimize import linprog
 
 #Paso 2. Crear una clase Principal Simplex
 class SimplexSolcion:
@@ -35,16 +37,84 @@ class SimplexSolcion:
         self.btn_pantalla=tk.Frame(self.ventana)
         self.btn_pantalla.grid(row=4,column=0,padx=10, pady=10)
 
-        self.btn_resolver=tk.Button(self.ventana,text="Resolver")
+        self.btn_resolver=tk.Button(self.ventana,text="Resolver", command=self.inciar_simplex)
         self.btn_resolver.grid(row=5,column=0,padx=10,pady=10)
 
-        self.btn_iteraciones=tk.Button(self.ventana, text="Iterar")
+        self.btn_iteraciones=tk.Button(self.ventana, text="Iterar", command=self.pasoApaso)
         self.btn_iteraciones.grid(row=5,column=1,padx=10)
 
         self.texto_Salida=tk.Text(self.ventana, height=20, width=100)
         self.texto_Salida.grid(row=6,column=0, padx=10,pady=10)
 
+    #Incializacion del programa
+    def inciar_simplex(self):
+        try:
+            c_entrada=self.entrada_funcion_objetivo.get().strip()
+            c = list(map(float, c_entrada.split()))
 
+            num_restricciones=int(self.entrada_num_restricciones.get().strip())
+
+            entrada_restricciones=self.entrada_restricciones.get("1.0",tk.END).strip().split('\n')
+
+            A=[list(map(float, line.split())) for line in entrada_restricciones]
+
+            b_entrada=self.entrada_igualdad.get().strip()
+            b=list(map(float, b_entrada.split()))
+
+            # Negar los coeficientes de la función objetivo para maximizar
+            c = [-coef for coef in c]
+            # Convertir A (Coeficientes de restricciones) y b (Coeficientes igualdad) en matrices 
+            A = np.array(A)
+            b = np.array(b)
+
+            # Inicializar la tabla simplex
+            self.num_variables = len(c)
+            self.num_filas = len(b)
+            self.tabla = np.hstack([A, np.eye(self.num_filas), b.reshape(-1, 1)])
+            self.c_completo = np.hstack([c, np.zeros(self.num_filas + 1)])
+            self.tabla = np.vstack([self.tabla, self.c_completo])
+
+            self.iteracion = 0
+            self.imprimir_Iteracion()
+
+            self.btn_iteraciones.config(state=tk.NORMAL)
+        except Exception as e:
+            messagebox.showerror("Error al resolver el problema")
+    
+    #Resolver el ejercicio paso a paso
+    def pasoApaso(self):
+        #Comprobar que se encontro la solucion optima
+        if all(i>=0 for i in self.tabla[0,:-1]):
+            self.texto_Salida.insert(tk.END, "\nSolución Óptima Encontrada.\n")
+            self.texto_Salida.insert(tk.END,f"Valor Óptimo: Z = {self.tabla[0, -1]}\n")
+
+            solucion=self.tabla[1:,-1]
+            self.texto_Salida.insert(tk.END,f"Valor de decision: {solucion[:self.num_variables]}\n")
+            self.btn_iteraciones.config(state=tk.DISABLED)
+            return
+
+            # Seleccionar la columna pivote
+        pivote_columna = np.argmin(self.tabla[0, :-1])  # Entrantes
+        if all(fila[pivote_columna] <= 0 for fila in self.tabla[1:]):
+            self.texto_Salida.insert(tk.END, "Error al resolver el problema")
+            self.btn_iteraciones.config(state=tk.DISABLED)
+            return
+        # Realizar el pivoteo
+        calculo = [fila[-1] / fila[pivote_columna] if fila[pivote_columna] > 0 else float('inf') for fila in self.tabla[1:]]  # Fila valores salientes
+        pivote_fila = np.argmin(calculo)
+        #Pivoteo
+        elemento_pivote = self.tabla[pivote_fila, pivote_columna]
+        self.tabla[pivote_fila] = self.tabla[pivote_fila] / elemento_pivote
+        for i in range(len(self.tabla)):
+            if i != pivote_fila:
+                self.tabla[i] = self.tabla[i] - self.tabla[i, pivote_columna] * self.tabla[pivote_fila]
+        self.iteracion += 1
+        self.imprimir_Iteracion()
+    
+    def imprimir_Iteracion(self):
+        self.texto_Salida.insert(tk.END, f"\nIteracion {self.iteracion} \n")
+        self.texto_Salida.insert(tk.END, "TABLA SIMPLEX \n")
+        self.texto_Salida.insert(tk.END,f"{self.tabla}\n")
 
 
 ventanaApp=tk.Tk()
